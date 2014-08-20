6620b6313c93f53aa531cb23166c9246750bb2eb
tobini2@gmail.com <tobini2@gmail.com@a09d15c9-8270-3bb2-8cbd-96ae0ad8e16e>
2014-08-20 15:52:36 +0000

    primer boceto. faltan 40 mil cosas.

---
+++ trunk/tp1-mn/src/sources/main.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+#define DEBUG
+
+#define UNDEFINED_TEMPERATURE 2048.0
+
+#ifdef DEBUG
+#define DEBUGMSG(X)  cout << X << endl;
+#else
+#define DEBUGMSG(X)
+#endif
+
+using namespace std;
+
+typedef Temp double;
+
+struct Point {
+	double x, y;
+	Point();
+	Point(const double posx, const double posy) : x(posx), y(posy) {};
+};
+
+class Parabrisas {
+	public:
+		Parabrisas();
+		~Parabrisas();
+		int read_from_input();
+		void calculate_temps();
+		void kill_leech();
+		void show();
+	private:
+		struct Leech {
+			int id;
+			Point position;
+			vector<Point> leeched_points;
+			
+			Leech();
+			Leech(const int id_leech, const Point p) : id(id_leech), points(position), leeched_points(Parabrisas.affected_points(p)) {};
+		};
+		
+		struct PB_Matrix {
+			vector< vector<Temp> > matrix;
+			int real_width, real_height;
+			
+			PB_Matrix(){
+				// En la creación de la estructura, guardo el n+1 m+1 reales resultantes de dividir el width y height por el intervalo.
+				this.real_width = Parabrisas.width / Parabrisas.discr_interval ;	//Techo, piso, round?
+				this.real_height = Parabrisas.height / Parabrisas.discr_interval ;
+				
+				this.matrix = vector(real_width, vector<Temp>(real_height, UNDEFINED_TEMPERATURE) );	//temperatura arbitraria para las no-calculadas.
+				
+				// Pongo los bordes en -100 (a revisar)
+				for( int i = 0; i < real_width; i++) {
+					this.matrix[0,i] = -100.0;
+					this.matrix[real_height,i] = -100.0;
+				}
+				
+				for (int j = 0; j < real_height; j++){
+					this.matrix[j,0] = -100.0;
+					this.matrix[j,real_width] = -100.0;
+				}
+			};
+			
+			Temp get(const Point p){
+				return this.matrix[p.x/Parabrisas.discr_interval][p.y/Parabrisas.discr_interval];
+			};
+		};
+		
+		static vector<Point> affected_points(const Point p);
+		void gaussian_elimination();
+		
+		static double width, height, discr_interval, radius, temp;
+		//int amount_of_leeches;	// No lo guardo porque se puede pedir el length de leeches, encima esto va a ir variando al matar sanguijuelas.
+		
+		vector<Leech> leeches;
+};
+
+Parabrisas::Parabrisas() { }
+
+Parabrisas::~Parabrisas() { }
+
+int Parabrisas::read_from_input() {
+	// Inicializo los valores a b h r t k.
+	int amount_of_leeches;
+	cin >> width >> height >> discr_interval >> radius >> temp >> amount_of_leeches;
+	
+	// Creo el vector de sanguijuelas. 
+	// Para cada sanguijuela creada veo los puntos afectados y los guardo en su propia estructura.
+	
+	for(int i = 0; i < amount_of_leeches; i++) {
+		double x, y;
+		cin >> x >> y;
+		
+		leeches.push(Leech(i, Point(x,y)));
+	}
+	
+	return 0;
+}
+
+void Parabrisas::calculate_temps() {
+
+}
+
+void Parabrisas::kill_leech() {
+
+}
+
+void Parabrisas::gaussian_elimination() {
+
+}
+
+vector<Point> Parabrisas::affected_points(const Point p){
+	/// QUERIA HACER ESTO
+	vector<Point> res;
+	return res;
+}
+
+void Parabrisas::show() {
+	/*if (best_path.size() == 0){
+		cout << "no" << endl;
+		return;
+	}
+	
+	// Pesos desde 0 se le habian sumado 1.
+	cout << best_weight_1 << " " << best_weight_2 << " " << best_path.size();
+	for(int i = 0; i < (int) best_path.size(); i++) {
+		// Nosotros numeramos desde 0 y ellos desde 1.
+		cout << " " << best_path[i] + 1;
+	}
+	cout << endl;*/
+}
+
+int main(int argc, char *argv[]) {
+	Parabrisas pb;
+	
+	pb.read_from_input();
+	pb.calculate_temps();
+	pb.show();
+	
+	return 0;
+}
+ 
