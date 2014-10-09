d9d205850bdfe5aad95d9f1052c9c6f700dcc579
marian.sabianaa <marian.sabianaa@a09d15c9-8270-3bb2-8cbd-96ae0ad8e16e>
2014-10-09 01:51:00 +0000

    Ejercicio 1 - PageRank, convergencia

---
+++ tp2-mn/src/abortion_page_rank_statistics_teletransport_zoom.bat
@@ -0,0 +1,11 @@
+@echo off
+.\tp.exe .\tests\benchmarks\rank\abortion10.in dummy.out
+.\tp.exe .\tests\benchmarks\rank\abortion12.in dummy.out
+.\tp.exe .\tests\benchmarks\rank\abortion15.in dummy.out
+.\tp.exe .\tests\benchmarks\rank\abortion18.in dummy.out
+.\tp.exe .\tests\benchmarks\rank\abortion21.in dummy.out
+.\tp.exe .\tests\benchmarks\rank\abortion24.in dummy.out
+.\tp.exe .\tests\benchmarks\rank\abortion27.in dummy.out
+.\tp.exe .\tests\benchmarks\rank\abortion30.in dummy.out
+
+
