af97484ae48ce8d100ea4b3a01f49044b3483ce4
tobini2@gmail.com <tobini2@gmail.com@a09d15c9-8270-3bb2-8cbd-96ae0ad8e16e>
2014-10-25 14:52:08 +0000

    todas las imagenes en txt asi ya pueden empezar y el script para hacerlo por las dudas (este script no es una funcion, llamenlo corriendo el archivo [ctrl+enter en el editor])

---
+++ tp3-mn/enunciado/mosaicing/save_images_as_txt.m
@@ -0,0 +1,24 @@
+%% Grabar imágenes en archivos
+
+root = '../images/';
+root_to_save = '../images_txt/';
+saving_format = '.txt';
+
+lookup_term = strcat(root, '*.bmp');
+listing_bmp_names = ls(lookup_term);
+
+for i = 1:size(listing_bmp_names, 1)
+    % Read image
+    img_file_name =  listing_bmp_names(i,:);
+    img_path = strcat(root, img_file_name);
+    im = imread(img_path);
+    
+    % Mosaicing
+    im_mosaic_to_save = mosaicing(im);
+    
+    % Saving
+    file_name_separate = strsplit(img_file_name, '.');
+    file_name_without_extension = file_name_separate(1);
+    saving_path_for_current_img = char(strcat(root_to_save, file_name_without_extension, saving_format));
+    save(saving_path_for_current_img, 'im_mosaic_to_save', '-ascii');
+end
