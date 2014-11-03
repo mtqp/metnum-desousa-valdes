73ffad1aae7c3e1d6da19e432735048dff727aa5
tobini2@gmail.com <tobini2@gmail.com@a09d15c9-8270-3bb2-8cbd-96ae0ad8e16e>
2014-11-03 12:33:45 +0000

    psnr corregido con mse por separado

--- tp3-mn/enunciado/matlab/psnr.m
+++ tp3-mn/enunciado/matlab/psnr.m
@@ -1,8 +1,6 @@
 function [ score_in_db ] = psnr( img1, img2 )
 %PSNR Peak Signal To Noise Ratio
-
-mean_square_error = (1/size(img1, 1)*size(img2, 2))*((img1+img2).^2);
-max_uint8 = 255;
-score_in_db = 20*log10(max_uint8) - 10*log10(mean_square_error);
-
+	mean_square_error = mse(img1, img2);
+	max_uint8 = 255;
+	score_in_db = 20*log10(max_uint8) - 10*log10(mean_square_error);
 end
