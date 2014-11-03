333fc3dc2de0a061736908cbb2fbe75106aedee6
tobini2@gmail.com <tobini2@gmail.com@a09d15c9-8270-3bb2-8cbd-96ae0ad8e16e>
2014-11-03 12:36:02 +0000

    mse corregido

--- tp3-mn/enunciado/matlab/mse.m
+++ tp3-mn/enunciado/matlab/mse.m
@@ -1,9 +1,12 @@
 function [ error ] = mse( img1, img2 )
 %MSE Mean Squared Error
+	% Casteos para trabajar con doubles y que no limiten los uint8
 	img1 = double(img1);
 	img2 = double(img2);
+	
+	% MSE
 	m = size(img1, 1);
 	n = size(img1, 2);
-	substract_and_square_matrix = (img1-img2).^2;
-	error = (1/m*n) * sum(substract_and_square_matrix(:));
+	substract_and_square_matrix = (img1 - img2) .^ 2;
+	error = (1/(m*n)) * sum(substract_and_square_matrix(:));
 end
