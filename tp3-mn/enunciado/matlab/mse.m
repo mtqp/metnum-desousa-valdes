32d6a7c9ab6beec090354375fd971e333c1f155c
tobini2@gmail.com <tobini2@gmail.com@a09d15c9-8270-3bb2-8cbd-96ae0ad8e16e>
2014-11-03 13:18:17 +0000

    psnr y correcciones para poder grabar la imagen bien. ssim no me corre

--- tp3-mn/enunciado/matlab/mse.m
+++ tp3-mn/enunciado/matlab/mse.m
@@ -7,6 +7,9 @@ function [ error ] = mse( img1, img2 )
 	% MSE
 	m = size(img1, 1);
 	n = size(img1, 2);
-	substract_and_square_matrix = (img1 - img2) .^ 2;
+	substract_and_square_matrix = zeros(m,n);
+	for i = 1:3,
+		substract_and_square_matrix = substract_and_square_matrix + (img1(:,:,i) - img2(:,:,i)) .^ 2;
+	end
 	error = (1/(m*n)) * sum(substract_and_square_matrix(:));
 end
