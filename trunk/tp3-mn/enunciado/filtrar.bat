de72b50014801953fc5ab8ecda840a0a57384309
tobini2@gmail.com <tobini2@gmail.com@a09d15c9-8270-3bb2-8cbd-96ae0ad8e16e>
2014-11-08 23:21:35 +0000

    tiempos

--- trunk/tp3-mn/enunciado/filtrar.bat
+++ trunk/tp3-mn/enunciado/filtrar.bat
@@ -4,12 +4,12 @@ SET alpha=0.5
 REM si anduvieran los dos fors............
 FOR /L %%G IN (1,1,12) DO (
 	tp.exe "images_files/img%%G" 0 0.5
-	move images_files\img%%G_out_* images_files\out\closest_neighbor\
+	REM move images_files\img%%G_out_* images_files\out\closest_neighbor\
 	tp.exe "images_files/img%%G" 1 0.5
-	move images_files\img%%G_out_* images_files\out\bilinear_interpolation\
+	REM move images_files\img%%G_out_* images_files\out\bilinear_interpolation\
 	tp.exe "images_files/img%%G" 2 0.5
-	move images_files\img%%G_out_* images_files\out\directional_interpolation\
+	REM move images_files\img%%G_out_* images_files\out\directional_interpolation\
 	tp.exe "images_files/img%%G" 3 0.5
-	move images_files\img%%G_out_* images_files\out\malvar_he_cutler\
+	REM move images_files\img%%G_out_* images_files\out\malvar_he_cutler\
 )
 GOTO :eof
